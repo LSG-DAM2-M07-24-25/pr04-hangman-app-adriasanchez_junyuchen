fa239f482d467da308ddc95f0598059620403a6d
xiaoyuu857 <junyuchen13@gmail.com>
2024-12-12 22:25:49 +0100

    logo created

--- app/src/main/java/com/adriajunyu/hangman/ui/views/MenuScreen.kt
+++ app/src/main/java/com/adriajunyu/hangman/ui/views/MenuScreen.kt
@@ -1,24 +1,34 @@
 package com.adriajunyu.hangman.ui.views
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.adriajunyu.hangman.R
 import com.adriajunyu.hangman.ui.viewmodels.GameViewModel
 
 @Composable
 fun MenuScreen(navigateToGame: (String) -> Unit) {
-    Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+    Column (modifier = Modifier
+        .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Spacer(modifier = Modifier.weight(1f))
         Text(text = stringResource(id = R.string.app_name), fontSize = 25.sp)
-        Spacer(modifier = Modifier.weight(1f))
+        Image(
+            painter = painterResource(id = R.drawable.hangman_7),
+            contentDescription = stringResource(id = R.string.app_name),
+            modifier = Modifier.size(250.dp)
+        )
         // Al llamar a la funcion navigateToGame, se navega a la pantalla de juego,
         // con la palabra "hello" como parametro
         Button(onClick = { navigateToGame(GameViewModel().getRandomWord()) }) {
