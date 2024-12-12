6136fcbdea05bef7733bc223addc0f0c74f51c21
adria <adria.sanchez.c@gmail.com>
2024-12-12 19:20:06 +0100

    feat: Version jugable sin texto en los botones

--- app/src/main/java/com/adriajunyu/hangman/ui/viewmodels/GameViewModel.kt
+++ app/src/main/java/com/adriajunyu/hangman/ui/viewmodels/GameViewModel.kt
@@ -6,11 +6,26 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel(new_word: String? = null) : ViewModel() {
     private val words = listOf("a", "ab", "abc", "abcd", "abcde")
+    val alphabet = listOf(
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',  'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    )
+
     private val _word = MutableLiveData<String>()
     val word : LiveData<String> = _word
 
     private val _hidenWord = MutableLiveData<String>()
     val hidenWord : LiveData<String> = _hidenWord
+
+    private val _win = MutableLiveData<Boolean>()
+    val win : LiveData<Boolean> = _win
+
+    private val _lose = MutableLiveData<Boolean>()
+    val lose : LiveData<Boolean> = _lose
+
+    private val _attempts = MutableLiveData<Int>()
+    val attempts : LiveData<Int> = _attempts
+
+    private val maximumAttempts = 5
 
     private val noLetter = '_'
 
@@ -36,18 +51,35 @@ class GameViewModel(new_word: String? = null) : ViewModel() {
     fun getRandomWord(): String = words.random().lowercase() ?: "android"
 
     fun showLetter(letter: Char) {
+        var correct = false
+
         val word = _word.value ?: ""
         val wordArray = word.toCharArray()
         val hidenWord = _hidenWord.value ?: ""
         val hidenWordArray = hidenWord.replace(" ", "").toCharArray()
-        var new_hidenWord = CharArray(hidenWordArray.size)
+        val new_hidenWord = CharArray(hidenWordArray.size)
+
         for (i in wordArray.indices) {
             if (wordArray[i].lowercase() == letter.lowercase()) {
                 new_hidenWord[i] = letter
+                correct = true
             } else {
                 new_hidenWord[i] = hidenWordArray[i]
             }
         }
+
         _hidenWord.value = new_hidenWord.joinToString(" ")
+
+        if (_word.value?.lowercase() == _hidenWord.value?.lowercase()?.replace(" ", "")) {
+            _win.value = true
+            return
+        }
+
+        if (!correct) {
+            _attempts.value = (_attempts.value ?: 0) + 1
+            if (_attempts.value == maximumAttempts) {
+                _lose.value = true
+            }
+        }
     }
 }
--- app/src/main/java/com/adriajunyu/hangman/ui/views/GameScreen.kt
+++ app/src/main/java/com/adriajunyu/hangman/ui/views/GameScreen.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,44 +16,60 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.adriajunyu.hangman.R
 import com.adriajunyu.hangman.ui.viewmodels.GameViewModel
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
 
 @Composable
 fun GameScreen(navigateToBack: () -> Unit, navigateToFinal: (Boolean, Int) -> Unit, viewModel: GameViewModel) {
     val hidenWord by viewModel.hidenWord.observeAsState("")
+    val win by viewModel.win.observeAsState(false)
+    val lose by viewModel.lose.observeAsState(false)
+    val attempts by viewModel.attempts.observeAsState(0)
+    val navigated = remember { mutableStateOf(false) }
 
     Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Spacer(modifier = Modifier.weight(1f))
         Text(text = stringResource(id = R.string.game_word), fontSize = 25.sp)
         Text(text = hidenWord, fontSize = 25.sp)
+        Text(text = stringResource(id = R.string.attempts, attempts), fontSize = 25.sp)
         Spacer(modifier = Modifier.weight(1f))
         Button(onClick = { navigateToBack() }) {
             Text(text = stringResource(id = R.string.back))
         }
         Spacer(modifier = Modifier.weight(1f))
-        Row (horizontalArrangement = Arrangement.SpaceEvenly) {
-            Spacer(modifier = Modifier.weight(1f))
-            // Al llamar a la funcion navigateToFinal, se navega a la pantalla final
-            // con el parametro win = false y attempts = 5
-            Button(onClick = { navigateToFinal(false, 5) }) {
-                Text(text = stringResource(id = R.string.lose))
+
+        if ((win || lose) && !navigated.value) {
+            navigated.value = true
+            navigateToFinal(win, attempts)
+        }
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(7),
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(viewModel.alphabet) { letter ->
+                LetterButton(letter = letter, enabled = true) {
+                    viewModel.showLetter(letter)
+                }
             }
-            Spacer(modifier = Modifier.weight(1f))
-            // Al llamar a la funcion navigateToFinal, se navega a la pantalla final
-            // con el parametro win = true y attempts = 2
-            Button(onClick = { navigateToFinal(true, 2) }) {
-                Text(text = stringResource(id = R.string.win))
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            Button( onClick = { viewModel.showLetter('a') } ) {
-                Text(text = "Mostrar A")
-            }
-            Spacer(modifier = Modifier.weight(1f))
         }
         Spacer(modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+fun LetterButton(letter: Char, enabled: Boolean, onClick: () -> Unit) {
+    Button(onClick = onClick, enabled = enabled) {
+        Text(text = letter.toString())
     }
 }
